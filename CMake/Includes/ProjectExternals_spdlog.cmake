95e5fbca76fa515c0405a0dc6d56ae350b998d79
GitHub Action <action@github.com>
2025-12-07 21:02:45 +0800

    Fix macOS spdlog build and add Linux artifact debug output

--- CMake/Includes/ProjectExternals_spdlog.cmake
+++ CMake/Includes/ProjectExternals_spdlog.cmake
@@ -14,17 +14,18 @@ if(APPLE)
         GIT_TAG ${spdlog-TAG}
         GIT_SHALLOW 1
 
-        CMAKE_ARGS "${CMAKE_ARGS};-DCMAKE_BUILD_TYPE=Release;-DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>;-DSPDLOG_BUILD_BENCH=off;-DSPDLOG_BUILD_EXAMPLES=off;-DSPDLOG_BUILD_TESTS=off;-DBUILD_TESTING=off;-DCMAKE_POLICY_VERSION_MINIMUM=3.5;-DSPDLOG_FMT_EXTERNAL=OFF;-DSPDLOG_BUILD_SHARED=OFF"
+        CMAKE_ARGS "${CMAKE_ARGS};-DCMAKE_BUILD_TYPE=Release;-DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>;-DSPDLOG_BUILD_BENCH=off;-DSPDLOG_BUILD_EXAMPLES=off;-DSPDLOG_BUILD_TESTS=off;-DBUILD_TESTING=off;-DCMAKE_POLICY_VERSION_MINIMUM=3.5;-DSPDLOG_FMT_EXTERNAL=OFF;-DSPDLOG_BUILD_SHARED=OFF;-DCMAKE_POSITION_INDEPENDENT_CODE=ON"
         CMAKE_COMMAND ${CMAKE_CROSS_COMMAND}
-        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/external/lib/libspdlog.a
+        BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --target spdlog
+        BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libspdlog.a
     )
     
     add_library(spdlog::spdlog STATIC IMPORTED)
     set_target_properties(spdlog::spdlog PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/external/include"
-        IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/libspdlog.a
+        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/external/lib/libspdlog.a"
     )
-    add_dependencies(spdlog::spdlog DEPENDS spdlog-${spdlog-TAG})
+    add_dependencies(spdlog::spdlog spdlog-${spdlog-TAG})
 else()
     ExternalProject_Add(spdlog-${spdlog-TAG}
         PREFIX ${CMAKE_BINARY_DIR}/external
